8178a0fa0cdf021ee902b2b355e214d353b5f5eb
Алексей Бахарев <alex.bakharew@yandex.ru>
2017-12-05 01:22:13 +0300

    some changes

---
+++ laba6/laba6/sources/queue.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include "queue.h"
+TQueue::TQueue()
+{
+	begin = end = nullptr;
+	size = 0;
+}
+bool TQueue::push(void* ptr)
+{
+	elem* tmp = new elem;
+	if (tmp == nullptr) return false;
+	if (size == 0)
+	{
+		begin = end = tmp;
+		tmp->next = tmp->prev = nullptr;
+		size++;
+		return true;
+	}
+	tmp->pointer = ptr;
+	begin->prev = tmp;
+	tmp->next = begin;
+	tmp->prev = nullptr;
+	begin = tmp;
+	size++;
+	return true;
+
+}
+void* TQueue::pop()
+{
+	elem* tmp = end;
+	if (size == 0) return nullptr;
+	if (size == 1)
+	{
+		void* tmp_ptr = tmp->pointer;
+		delete tmp;
+		begin = end;
+		size--;
+		return tmp_ptr;
+	}
+	end = end->prev;
+	end->next = nullptr;
+	void* tmp_ptr = tmp->pointer;
+	delete tmp;
+	size--;
+	return tmp_ptr;
+}
+bool TQueue::is_empty()
+{
+	return size > 0;
+}
+TQueue::~TQueue()
+{
+	while (size != 0)
+	{
+		pop();
+	}
+}
---
+++ laba6/laba6/sources/triangle.cpp
@@ -0,0 +1,46 @@
+#include"../headers/stdafx.h"
+//#include "../headers/stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include "../headers/triangle.h"
+#include <cmath>
+Triangle::Triangle() : Triangle(0, 0, 0) {};
+Triangle::Triangle(size_t i, size_t j, size_t k) : side_a(i), side_b(j), side_c(k)
+{
+	std::cout << "Triangle was created successfully" << std::endl;
+}
+
+Triangle::Triangle(std::istream &is)
+{
+	std::cout << "Creation of triangle" << std::endl;
+	std::cout << "Enter the length of 3 sides" << std::endl;
+	is >> side_a;
+	is >> side_b;
+	is >> side_c;
+	std::cout << "Triangle was created successfully" << std::endl;
+}
+Triangle::Triangle(const Triangle& orig)
+{
+	side_a = orig.side_a;
+	side_b = orig.side_b;
+	side_c = orig.side_c;
+	std::cout << "Triangle was copied successfully!" << std::endl;
+}
+Triangle::~Triangle()
+{
+	std::cout << "Triangle was deleted" << std::endl;
+}
+double Triangle::Square()
+{
+	double p = double(side_a + side_b + side_c) / 2;
+	return sqrt(p*(p - side_a)*(p - side_b)*(p - side_c));
+}
+double Triangle::Print()
+{
+	std::cout << "Here is a TRIANGLE with following sides:" << std::endl;
+	std::cout << "side a = " << side_a << std::endl;
+	std::cout << "side b = " << side_b << std::endl;
+	std::cout << "side c = " << side_c << std::endl;
+	std::cout << "Square = ";
+	return Square();
+}
